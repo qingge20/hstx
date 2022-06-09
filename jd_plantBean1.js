652a36fe7aa4f49bdc3a6399e169f86ea5cc56c6
qingge20 <76232945+qingge20@users.noreply.github.com>
2022-06-09 22:20:30 +0800

    种豆得豆一

--- jd_plantBean1.js
+++ jd_plantBean1.js
@@ -17,9 +17,9 @@
 cron "1 7-21/2 * * *" script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_plantBean.js,tag=京东种豆得豆
 
 ======================================Surge==========================
-京东种豆得豆 = type=cron,cronexp="1 7-21/2 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_plantBean.js
-
-====================================小火箭=============================
+京东种豆得豆 = type=cron,cronexp="1 7-21/2 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/KingRan/ JDJB/main/jd_plantBean.js
+吗
+=====================================小火箭=============================
 京东种豆得豆 = type=cron,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_plantBean.js, cronexpr="1 7-21/2 * * *", timeout=3600, enable=true
 
 */
