42acc9cbc56ae4f160afbbfdb7d546cc378b3af7
qingge20 <76232945+qingge20@users.noreply.github.com>
2022-08-04 22:14:33 +0800

    东东蒙宠2

--- jd_pet1.js
+++ jd_pet1.js
@@ -1,6 +1,6 @@
 /*
 #City脚本配置
-cron "15 5-10/1 * * *" script-path=jd_pets.js,tag=东东萌宠
+cron "15 6-12/3 * * *" script-path=jd_pets.js,tag=东东萌宠
 */
 const $ = new Env('东东萌宠');
 let cookiesArr = [], cookie = '', allMessage = '';
